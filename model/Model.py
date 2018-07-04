0bd30bba4c7c19088c4b37ad22f7046e82efd4eb
Leerw <lee.rw.cs@gmail.com>
2018-07-04 10:39:23 +0800

    ui

--- model/Model.py
+++ model/Model.py
@@ -1,6 +1,7 @@
 import mysql
 import pymysql
 
+
 class Model:
 
     def __init__(self):
@@ -21,6 +22,21 @@ class Model:
         pass
 
     def result_cal(self, times):
+        # arrive_time[i] = sum(interval_time[0..i-1])
+        for i in range(0, )
         pass
 
-    
+    def data_pool(self):
+        return {self.arrive_time, self.interval_time,
+                self.serve_time, self.serve_start_time,
+                self.wait_time, self.serve_end_time,
+                self.spend_time, self.sys_free_time,
+                self.avg_wait_time, self.sys_util}
+
+    def connect_db(self):
+        return pymysql.connect(host='127.0.0.1',
+                               port=3306,
+                               user='root',
+                               password='Qaz520..',
+                               database='xxq',
+                               charset='utf8')
