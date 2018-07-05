57df7147818c2161489b0139f807c71146120642
Leerw <lee.rw.cs@gmail.com>
2018-07-05 09:14:01 +0800

    create DB

--- createDB.sh
+++ createDB.sh
@@ -1,2 +1,21 @@
 #!/bin/sh
-PATH=
+MYUSER=root
+MYPASS=Qaz520..
+DBNAME=xxq
+mysql -u $MYUSER -p$MYPASS
+CREATE DATABSAE $DBNAME
+USE $DBNAME
+CREATE TABLE IF NOT EXISTS serve(
+    client_id INT UNSIGNED NOT NULL,
+    arrive_time INT UNSIGNED,
+    interval_time INT UNSIGNED,
+    serve_time INT UNSIGNED,
+    serve_start_time INT UNSIGNED,
+    wait_time INT UNSIGNED,
+    serve_end_time INT UNSIGNED,
+    spend_time INT UNSIGNED,
+    sys_free_time INT UNSIGNED,
+    avg_wait_time INT_UNSIGNED,
+    sys_util INT UNSIGNED,
+    PRIMARY KEY (client_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8
--- model/Model.py
+++ model/Model.py
@@ -21,7 +21,7 @@ class Model:
         pass
 
     def data_gen(self, num, max, min, probabilities):
-        #下面是生成到达时间的随机值
+        # 下面是生成到达时间的随机值
         tmp = True
         y = []
         lam = (max + min) / 2
@@ -41,9 +41,9 @@ class Model:
         y.sort()
         self.arrive_time = y
 
-        #下面是生成服务时间的随机值
-        seq = [1,2,3,4,5,6]
-        while len(self.serve_time)!=num:
+        # 下面是生成服务时间的随机值
+        seq = [1, 2, 3, 4, 5, 6]
+        while len(self.serve_time) != num:
             z = random.uniform(0, 1)
             cumprob = 0.0
             for item, item_pro in zip(seq, probabilities):
@@ -52,40 +52,40 @@ class Model:
                     break
             self.serve_time.append(item)
 
-
-
-
-
-    def result_cal(self,num):
-        #对第一次进行数据添加
+    def result_cal(self, num):
+        # 对第一次进行数据添加
         self.wait_time.append(0)
         self.serve_end_time.append(self.serve_time[0]+self.arrive_time[0])
         self.interval_time.append(0)
         self.serve_start_time.append(0)
-        #对后面的进行数据添加
-        for i in range(1,num):
-            wait_time = self.serve_end_time[i- 1] - self.arrive_time[i]#等待时间=上一次的结束时间-到达时间
+        # 对后面的进行数据添加
+        for i in range(1, num):
+            wait_time = self.serve_end_time[i - 1] - \
+                self.arrive_time[i]  # 等待时间=上一次的结束时间-到达时间
             if wait_time < 0:
-                self.sys_free_time.append(wait_time)#等待时间如果为0，第i个顾客的系统空闲时间就不为0
+                # 等待时间如果为0，第i个顾客的系统空闲时间就不为0
+                self.sys_free_time.append(wait_time)
                 wait_time = 0
             else:
                 self.sys_free_time.append(0)
             self.wait_time.append(wait_time)
             self.serve_end_time.append(self.arrive_time[i]+self.wait_time[i]+self
-                                       .serve_time[i])#结束时间=等待时间+到达时间+服务时间
+                                       .serve_time[i])  # 结束时间=等待时间+到达时间+服务时间
         wait_time = 0
         serve_time = self.serve_time[0]
-        for i in range(1,num):
-            self.serve_start_time.append(self.arrive_time[i]+ self.wait_time[i])#开始时间=到达时间+等待时间
-            self.spend_time.append(self.wait_time[i]+self.serve_time[i])#花费时间 = 等待时间+服务时间
-            self.interval_time.append(self.arrive_time[i] - self.arrive_time[i-1])#间隔时间 = 这次到达时间-上次到达时间
+        for i in range(1, num):
+            self.serve_start_time.append(
+                self.arrive_time[i] + self.wait_time[i])  # 开始时间=到达时间+等待时间
+            self.spend_time.append(
+                self.wait_time[i]+self.serve_time[i])  # 花费时间 = 等待时间+服务时间
+            self.interval_time.append(
+                self.arrive_time[i] - self.arrive_time[i-1])  # 间隔时间 = 这次到达时间-上次到达时间
             wait_time = wait_time + self.wait_time[i]
-            serve_time =serve_time + self.serve_time[i]
-        self.avg_wait_time = wait_time / num  #系统平均等待时间是等待时间之和除以人数
-        all_time = self.serve_end_time[num-1] - self.arrive_time[0] #系统总运行时间 = 最后一个人的结束时间 - 第一个人的到达时间
-        self.sys_util = serve_time /all_time #系统利用率 = 系统服务总时间 / 系统总运行时间
-
-
+            serve_time = serve_time + self.serve_time[i]
+        self.avg_wait_time = wait_time / num  # 系统平均等待时间是等待时间之和除以人数
+        # 系统总运行时间 = 最后一个人的结束时间 - 第一个人的到达时间
+        all_time = self.serve_end_time[num-1] - self.arrive_time[0]
+        self.sys_util = serve_time / all_time  # 系统利用率 = 系统服务总时间 / 系统总运行时间
 
     def data_pool(self):
         return {self.arrive_time, self.interval_time,
@@ -107,10 +107,15 @@ class Model:
             total_prob = total_prob + every_prob
         return total_prob == 1
 
+
     def connect_db(self):
-        return pymysql.connect(host='127.0.0.1',
-                               port=3306,
-                               user='root',
-                               password='Qaz520..',
-                               database='xxq',
-                               charset='utf8')
+        config = {'host': '127.0.0.1',
+                  'port': 3306,
+                  'user': 'root',
+                  'password': 'Qaz520..',
+                  'database': 'xxq',
+                  'charset': 'utf8'
+                  }
+        return pymysql.connect(config)
+
+    def 
