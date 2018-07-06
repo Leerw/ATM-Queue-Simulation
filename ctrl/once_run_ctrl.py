8d74f97cf25eabd3c6a4cefcd661df113ce94460
hbq123 <1265435249@qq.com>
2018-07-06 11:02:37 +0800

    运行一次的表格填充

--- ctrl/once_run_ctrl.py
+++ ctrl/once_run_ctrl.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QTableWidgetItem, QDialog
 from view.once_run_view import Ui_OnceRun
+from model.Model import Model
 
 
 class OnceRunCtrl(Ui_OnceRun):
@@ -10,20 +11,41 @@ class OnceRunCtrl(Ui_OnceRun):
         self.help_btn.clicked.connect(self.help)
 
     def clr_cache(self):
-        self.once_run_table.clearContents()
+        while self.once_run_table.rowCount() != 0:
+            self.once_run_table.removeRow(0)
+        self.once_run_table.insertRow(0)
+        item = QTableWidgetItem()
+        item.setText("**")
+        self.once_run_table.setVerticalHeaderItem(0, item)
+        self.sys_use_txt.clear()
+        self.avg_txt.clear()
 
     def finish(self):
         self.close()
         exit(0)
 
     def once_run(self):
+        self.clr_cache()
+        m = Model()
+        m.data_gen(10, 5, 12, [0.1, 0.1, 0.2, 0.2, 0.2, 0.2])
+        m.result_cal(10)
+        r = m.data_pool()
         col_count = self.once_run_table.columnCount()
-        for i in range(col_count):
+        self.once_run_table.removeRow(0)
+        for j in range(len(r[0]) - 1):
+            self.once_run_table.insertRow(j)
             item = QTableWidgetItem()
-            item.setText("111")
-            self.once_run_table.setItem(0, i, item)
-        self.avg_txt.setText("111")
-        self.sys_use_txt.setText("111")
+            item.setText(str(j + 1))
+            self.once_run_table.setVerticalHeaderItem(j, item)
+            item = QTableWidgetItem()
+            item.setText(str(j + 1))
+            self.once_run_table.setItem(j, 0, item)
+            for i in range(1, col_count):
+                item = QTableWidgetItem()
+                item.setText(str(r[i - 1][j]))
+                self.once_run_table.setItem(j, i, item)
+        self.avg_txt.setText(str(r[8]))
+        self.sys_use_txt.setText(str(r[9]))
 
     def help(self):
         d = QDialog()
