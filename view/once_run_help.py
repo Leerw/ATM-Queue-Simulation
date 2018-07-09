847a44d1d341aa0709b54a53302fe18d894c80cc
Adonis-Song <1922289549@qq.com>
2018-07-09 10:54:57 +0800

    一次运行帮助界面更改

--- view/once_run_help.py
+++ view/once_run_help.py
@@ -11,12 +11,12 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(400, 300)
+        Dialog.resize(424, 311)
         self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(150, 90, 72, 15))
+        self.label.setGeometry(QtCore.QRect(20, 60, 391, 131))
         self.label.setObjectName("label")
         self.verifty = QtWidgets.QPushButton(Dialog)
-        self.verifty.setGeometry(QtCore.QRect(130, 190, 93, 28))
+        self.verifty.setGeometry(QtCore.QRect(160, 200, 93, 28))
         self.verifty.setObjectName("verifty")
 
         self.retranslateUi(Dialog)
@@ -25,6 +25,6 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "TextLabel"))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p>点击“运行一次”开始运行，并在显示框中显示相应的数据</p><p>点击“清除数据缓存”，所有数据清空</p><p><br/></p></body></html>"))
         self.verifty.setText(_translate("Dialog", "确定"))
 
